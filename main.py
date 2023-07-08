525bb05cb674f8bb346ba3775f8537bf1e558ea7
ValeraDX <ValeraDX@yandex.ru>
2023-07-08 18:19:27 +0300

    Added the Get 101 Save button that functions and changed buttons a bit.

--- main.py
+++ main.py
@@ -2,31 +2,6 @@ import requests
 import tkinter as tk
 import ttkbootstrap as ttk
 from ttkbootstrap.constants import *
-
-
-def getfullsavefile(path:str = "fullsavefile.ini"):
-    full_savefile = requests.get(
-        "https://gist.githubusercontent.com/FexinShifter/821845203c24ad3d6d527471e5b4a9e7/raw/fcee0d0c84736a67031ee9664d48a41573225a4d/101%2525",
-        stream=True)
-    file = open(path, "wb")
-    file.write(full_savefile.content)
-    file.close()
-
-
-def maintui():
-    choice = input('To get 101% Pizza Tower Save File, type "101", or "exit" to quit. \n>>> ')
-
-    if choice == "101":
-        getfullsavefile()
-    elif choice == "exit":
-        print("Exiting...")
-        exit(0)
-    else:
-        print(f'Invalid argument "{choice}"!')
-        maintui()
-
-
-
 class gui(ttk.Frame):
     def __init__(self,master):
         super().__init__(master)
@@ -35,11 +10,21 @@ class gui(ttk.Frame):
     def create_gui(self):
         container=ttk.Frame(self,padding=10)
         container.pack(fill=X)
-        self.buttons=[ttk.Button(master=container,text="exit",bootstyle=PRIMARY, command=self.onclick)]
+        self.buttons=[ttk.Button(master=container,text="Get 101% Save File",bootstyle=PRIMARY, command=self.getfullsavefile),
+                      ttk.Button(master=container,text="Exit",bootstyle="DANGER OUTLINE", command=self.onclick),
+                      ]
         for button in self.buttons:
             button.pack(side=LEFT, fill=X, expand=YES, pady=10, padx=5)
     def onclick(self):
         self.quit()
+
+    def getfullsavefile(self, path: str = "fullsavefile.ini"):
+        full_savefile = requests.get(
+            "https://gist.githubusercontent.com/FexinShifter/821845203c24ad3d6d527471e5b4a9e7/raw/fcee0d0c84736a67031ee9664d48a41573225a4d/101%2525",
+            stream=True)
+        file = open(path, "wb")
+        file.write(full_savefile.content)
+        file.close()
 
 # Workaround that helps to use this script as a module.
 if __name__ == '__main__':
